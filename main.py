950a68bcaca4e8aa406e9b621ff7c589d98923f4
AK <1688497+mahacoder@users.noreply.github.com>
2025-07-02 15:58:40 +0100

    Add a simple class

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello World')
+class FooBar:
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+def main():
+    foo_bar = FooBar(42)
+    print("Initial value:", foo_bar.get_value())
+    
+    foo_bar.set_value(100)
+    print("Updated value:", foo_bar.get_value())
+
+if __name__ == "__main__":
+    main()
